832ebfe1ffb59f9eb6bb02dd4af73f2f55e3c523
lawrenceee04 <lawrencelg04@gmail.com>
2024-08-18 11:11:32 +0800

    removed date.js file

--- js/date.js
+++
@@ -1,8 +0,0 @@
-function updateDate() {
-  const now = new Date();
-  const options = { year: "numeric", month: "long", day: "numeric" };
-  const dateString = now.toLocaleDateString(undefined, options);
-  document.getElementById("date").textContent = dateString;
-}
-
-updateDate();
